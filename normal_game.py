62bc87b7361bfffda00f4377f981b7179196d0ea
wlhberesford <wlhberesford@gmail.com>
2024-02-02 16:40:48 -0500

    filled in Normal Game Methods

--- normal_game.py
+++ normal_game.py
@@ -4,4 +4,20 @@ class Normal_Game:
     def __init__(self):
         self.players=[]
         self.strats=[]
-        self.payoff=[]
+        self.payoffs=[]
+
+    #---  ACCESORS & MUTATORS  ---#
+    def get_players(self):
+        return self.players
+    
+    def get_strats(self):
+        return self.strats    
+    
+    def get_payoffs(self):
+        return self.payoffs
+    
+    def add_player(self, player):
+        self.players.append(player)
+    
+    def remove_player(self, player):
+        self.players.remove(player)
